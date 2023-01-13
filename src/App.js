7bb2dcde397139b2feb7743c4e75ab27e8f8295c
bonveeee <bonochieng@gmail.com>
2023-01-13 22:43:31 +0300

    implemented POST request

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AddNote from "./components/AddNote";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
@@ -5,15 +6,39 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
+ //add Note
+ const addNote = async (note) => {
+  const res = await fetch("http://localhost:5000/notes", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
  
+  //new data is added
+  const data = await res.json();
+  setNotes([...notes, data]);
+ };
+
+//delete Task
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: "DELETE",
+  });
+
+  setNotes(notes.filter((note) => note.id !== id));
+};
+
   return (
     <Router>
       <div className="container">
         <Header
           title="Note Tracker"
         />
-        <AddNote/>
+        <AddNote onAdd={addNote}/> 
         <Routes>
        <Route path='/about' element={<About />} />
          </Routes>
--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {Form, Button} from 'react-bootstrap';
 
 const AddNote = ({ onAdd }) => {
   const [title, setTitle] = useState("");
@@ -12,7 +13,18 @@ const AddNote = ({ onAdd }) => {
     if (!content) {
       alert("Please add content for the note");
       return;
-    }
+     
+  }
+
+      if (!title) {
+      alert("Please add a title for the note");
+      return;
+      }
+      
+      if (!category) {
+      alert("Please add a category for the note");
+      return;
+      }
 
     onAdd({ title, content, category, reminder });
 
@@ -23,43 +35,36 @@ const AddNote = ({ onAdd }) => {
   };
 
   return (
-    <form className="add-note-form" onSubmit={onSubmit}>
-      <div className="form-control">
-        <label>Title</label>
-        <input
-          type="text"
-          placeholder="Add Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div className="form-control">
-        <label>Content</label>
-        <textarea
-          placeholder="Add Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-      </div>
-      <div className="form-control">
-        <label>Category</label>
-        <input
-          type="text"
-          placeholder="Add Category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-      </div>
-      <div className="form-control">
-        <label>Reminder:</label>
-        <input
-          type="checkbox"
-          checked={reminder}
-          onChange={(e) => setReminder(e.target.checked)}
-        />
-      </div>
-      <button type="submit">Add Note</button>
-    </form>
+    <Form  onSubmit={onSubmit}>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form.Label>Add Title</Form.Label>
+      <Form.Control type="text" placeholder="title"  value={title}  onChange={(e) => setTitle(e.target.value)}/>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+      <Form.Label>Add Note</Form.Label>
+      <Form.Control as="textarea" rows={3}  placeholder="write your note"  value={content}  onChange={(e) => setContent(e.target.value)}/>
+    </Form.Group>
+
+    <Form.Label>Category</Form.Label>
+    <Form.Select aria-label="Default select example" value={category}  onClick={(e) => setCategory(e.target.value)}>
+      <option>Open this select menu</option>
+      <option value="Work">Work</option>
+      <option value="Personal">Personal</option>
+      <option value="Other">Other</option>
+    </Form.Select>
+
+    <Form.Label>Reminder</Form.Label>
+    <Form.Check 
+        type="switch"
+        id="custom-switch"
+        value={reminder}
+        label="On"
+        onChange={(e) => setReminder(e.currentTarget.checked)}
+      />
+      <Button as="input" type="submit" value="Submit" />{' '}
+  </Form>
+  
   );
 };
 

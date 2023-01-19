30593bf0d614dac1743acaaa802bf4ee79ac4154
bonveeee <bonochieng@gmail.com>
2023-01-19 18:49:53 +0300

    [updated] About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,37 @@
-const About = () => {
+import { Button } from "react-bootstrap";
+
+const AboutUs = () => {
   return (
-        <>
+    <>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Welcome to Our Blogging Company</h1>
+          <p className="lead">
+            We are a team of passionate bloggers who believe in sharing the
+            untold stories behind the story.
+          </p>
+        </div>
+      </div>
 
+      <div className="container">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          sollicitudin, velit vel maximus tempor, sapien nibh feugiat risus, vel
+          tincidunt magna ligula eget est. Sed accumsan euismod risus, id
+          tincidunt magna.
+        </p>
+        <Button
+          href="/contact"
+          style={{ marginBottom: "10px", marginTop: "10px" }}
+          size="lg"
+          variant="outline-dark"
+          className="text-center"
+        >
+          Connect With Us
+        </Button>
+      </div>
+    </>
+  );
+};
 
-          
-        </>
-  )
-}
-
-export default About
+export default AboutUs;

647dcc1d7f5b456283642516dad1facd450cda97
bonveeee <bonochieng@gmail.com>
2023-01-22 22:14:59 +0300

    [minor]change

--- src/components/RecentPost.js
+++ src/components/RecentPost.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+
+//sort the posts
+//slice to get the last previous posts
+//map in a card or container
 const RecentPost = () => {
+
+
   return (
+
+
     <div>RecentPost</div>
   )
 }
